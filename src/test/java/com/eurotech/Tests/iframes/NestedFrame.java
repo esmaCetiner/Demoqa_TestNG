92b2bb2de3c34f6eabb19f77fbb6e93fa14a2ab8
zkartal <kartalzafer52@gmail.com>
2023-10-17 15:36:07 +0200

    one class "NestedFrame" uploaded

--- src/test/java/com/eurotech/Tests/iframes/NestedFrame.java
+++ src/test/java/com/eurotech/Tests/iframes/NestedFrame.java
@@ -10,7 +10,7 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class NestedFrames {
+public class NestedFrame {
     WebDriver driver;
 
     @BeforeMethod
